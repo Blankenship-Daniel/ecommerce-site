a74ba8542be33bca5848973e43c2e94572e900d7
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-17 19:30:17 -0600

    added ecommerce polymer component skeletons

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,5 +1,7 @@
 </body>
 <!-- place javascripts here -->
 <script src="bower_components/webcomponentsjs/webcomponents.min.js"></script>
+<script src="/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="js/app.js"></script>
 </html>
--- layouts/header.php
+++ layouts/header.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <title>Ecommerce Site</title>
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="styles/css/main.css">
         <link rel="import" href="bower_components/polymer/polymer.html">
         <link rel="import" href="components/elements.html">
-        <link rel="stylesheet" href="styles/css/main.css">
     </head>
     <body>
