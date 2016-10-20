6ed2141afb9ac3449fccedbc12d1692d499288e6
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-20 16:29:05 -0600

    started styling site

--- app/index.php
+++ app/index.php
@@ -1,5 +1,15 @@
 <?php require_once('../layouts/header.php') ?>
+<nav>
+    <span id="logo">
+        <i class="fa fa-gamepad"></i>
+        Retro Gaming
+    </span>
+</nav>
+<slide-show></slide-show>
 <div class="content">
     <h1>Ecommerce Site</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
 </div>
 <?php require_once('../layouts/footer.php') ?>
--- layouts/header.php
+++ layouts/header.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <title>Ecommerce Site</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="styles/css/main.css">
         <link rel="import" href="bower_components/polymer/polymer.html">
