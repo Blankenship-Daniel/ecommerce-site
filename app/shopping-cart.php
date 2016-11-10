893c0ec787f8e3828cf9f8af0665450ffbe5e696
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-11-10 08:58:20 -0700

    updating repo

---
+++ app/shopping-cart.php
@@ -0,0 +1,9 @@
+<?php require_once('header.php') ?>
+<div class="primary-color">
+    <div class="container heading-panel">
+        <div class="content">
+            <shopping-cart></shopping-cart>
+        </div>
+    </div>
+</div>
+<?php require_once('footer.php') ?>
