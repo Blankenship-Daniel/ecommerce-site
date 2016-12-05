2e75938f14d54a9048a8c920d83fbb9bcb0ebb50
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-12-05 09:17:20 -0700

    added about us info

--- app/index.php
+++ app/index.php
@@ -7,7 +7,26 @@
 <div class="lttp-img">
     <div class="container">
         <div class="content text-white">
-
+            <h1>Quality Gaming Guaranteed</h1>
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <h4>Return Policy</h4>
+                    <p>
+                        If you notice in glitches in the cartridge in the first
+                        30 days, return it for a full refund. Guaranteed.
+                    </p>
+                    <h4>Legal</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <h4>About Us</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
